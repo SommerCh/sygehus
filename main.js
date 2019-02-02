d07c12875c784e66dd0075aacc7035bbec96729d
SommerCh <47107441+SommerCh@users.noreply.github.com>
2019-02-02 12:58:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $("button").click(function () {
+        $("#right,nav").toggle(2000);
+    });
+    
+      $("button").click(function () {
+        $("#show").css({
+            "left": "0%"
+        });
+    });
+});
+
+
